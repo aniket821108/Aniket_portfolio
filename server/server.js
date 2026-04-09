3ea8fcafc3d2bad421c1fd5dd404cf10168c2932
aniket821108 <aniketkumar821108@gmail.com>
2026-04-09 19:21:58 +0530

    Fix proxy, remove static serve, switch to secure SMTP port

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const errorHandler = require('./middleware/errorHandler');
 connectDB();
 
 const app = express();
-
+app.set('trust proxy', 1);
 // Security & parsing
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
@@ -63,12 +63,12 @@ app.get('/api/health', (req, res) =>
 );
 
 // Serve React in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+//   );
+// }
 
 // Global error handler
 app.use(errorHandler);
